d0bb7e8896054f47c4d065102eb13445356bb755
shekhargrace14 <shekhargrace14@gmail.com>
2022-10-07 23:44:39 +0530

    Initial commit

---
+++ script copy.js
@@ -0,0 +1,23 @@
+const data = [
+    {
+        family:     "family1",
+        father:	    "Ramesh",
+        mother:	    "Radha",
+        daughter:	"Richa",
+        son:	    "Raj"
+    },
+    {
+        family:     "family2",
+        father:	    "Kishan",
+        mother:	    "kirti",
+        daughter:	"Kiran",
+        son:	    "Kamal"
+    },
+    {
+        family:     "family3",
+        father:	    "Pankaj",
+        mother:	    "Payal",
+        daughter:	"Priya",
+        son:	    "Paras"
+    },
+]
